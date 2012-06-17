6dcffeb2fce9ae4c15d7d2f40f8377700818c30c
Stefan Ferstl <st.ferstl@gmail.com>
2012-06-17 16:14:52 +0200

    first simple implementation of PedanticPluginConfigurationReader

--- src/main/java/com/github/ferstl/maven/pomenforcers/PedanticPluginConfigurationEnforcer.java
+++ src/main/java/com/github/ferstl/maven/pomenforcers/PedanticPluginConfigurationEnforcer.java
@@ -1,19 +1,42 @@
 package com.github.ferstl.maven.pomenforcers;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
 import org.apache.maven.enforcer.rule.api.EnforcerRuleHelper;
-import org.apache.maven.project.MavenProject;
+import org.apache.maven.model.Plugin;
 import org.w3c.dom.Document;
 
-import com.github.ferstl.maven.pomenforcers.util.EnforcerRuleUtils;
+import com.github.ferstl.maven.pomenforcers.reader.DeclaredPluginsReader;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
 
+// project.getParent().getFile() == null, if parent POM not available (i.e. in local repo)
+// project.getParent() != null, if there is a parent
+//
+// rule: if project has a parent
+//       -> plugin version has to be declared in parent's <pluginManagement>
+//       -> plugin configuration has to be defined in parent's <pluginManagement>
+//          EXCEPT the configuration is declared in within an <execution>
 public class PedanticPluginConfigurationEnforcer extends AbstractPedanticEnforcer {
 
   @Override
   protected void doEnforce(EnforcerRuleHelper helper, Document pom) throws EnforcerRuleException {
-    MavenProject mavenProject = EnforcerRuleUtils.getMavenProject(helper);
+    List<Plugin> declaredPlugins = new DeclaredPluginsReader(pom).read();
 
-    System.out.println("plugin-management" + mavenProject.getPluginManagement().getPlugins());
+    Collection<Plugin> versionedPlugins = Collections2.filter(declaredPlugins, new Predicate<Plugin>() {
+      @Override
+      public boolean apply(Plugin input) {
+        return input.getVersion() != null;
+      }
+    });
+
+    if (versionedPlugins.size() != 0) {
+      throw new EnforcerRuleException("One does not simply declare plugin versions! "
+          + "The versions of these plugins have to be configured in the <pluginManagement> section: "
+          + versionedPlugins);
+    }
   }
 
   @Override
---
+++ src/main/java/com/github/ferstl/maven/pomenforcers/reader/DeclaredPluginsReader.java
@@ -0,0 +1,45 @@
+package com.github.ferstl.maven.pomenforcers.reader;
+
+import java.util.List;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.apache.maven.model.Plugin;
+import org.w3c.dom.Document;
+
+import com.google.common.collect.Lists;
+import com.thoughtworks.xstream.XStream;
+
+public class DeclaredPluginsReader extends AbstractPomSectionReader<List<Plugin>> {
+
+  private static final String PLUGINS_XPATH = "/project/build/plugins";
+  private static final String PLUGINS_ALIAS = "plugins";
+  private static final String PLUGIN_ALIAS = "plugin";
+
+  public DeclaredPluginsReader(Document pom) {
+    super(pom);
+  }
+
+  @Override
+  protected XPathExpression createXPathExpression(XPath xpath) throws XPathExpressionException {
+    return xpath.compile(PLUGINS_XPATH);
+  }
+
+  @Override
+  protected void configureXStream(XStream xstream) {
+    xstream.alias(PLUGINS_ALIAS, List.class);
+    xstream.alias(PLUGIN_ALIAS, Plugin.class);
+    xstream.omitField(Plugin.class, "configuration");
+    xstream.omitField(Plugin.class, "extensions");
+    xstream.omitField(Plugin.class, "goals");
+    xstream.omitField(Plugin.class, "executions");
+    xstream.omitField(Plugin.class, "dependencies");
+  }
+
+  @Override
+  protected List<Plugin> getUndeclaredSection() {
+    return Lists.newArrayListWithCapacity(0);
+  }
+}
