ce2a62908e4085c9ef36de0b7a6651fe243678c1
Stefan Ferstl <st.ferstl@gmail.com>
2017-11-04 16:50:15 +0100

    Test also with Maven 3.3.9

--- src/test/java/com/github/ferstl/maven/pomenforcers/PedanticPomEnforcersIntegrationTest.java
+++ src/test/java/com/github/ferstl/maven/pomenforcers/PedanticPomEnforcersIntegrationTest.java
@@ -11,7 +11,7 @@ import io.takari.maven.testing.executor.MavenVersions;
 import io.takari.maven.testing.executor.junit.MavenJUnitTestRunner;
 
 @RunWith(MavenJUnitTestRunner.class)
-@MavenVersions({"3.5.0"})
+@MavenVersions({"3.5.0", "3.3.9"})
 public class PedanticPomEnforcersIntegrationTest {
 
   @Rule
