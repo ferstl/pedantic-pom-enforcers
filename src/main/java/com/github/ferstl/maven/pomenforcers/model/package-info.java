6ae42aa23be4961322426727659dce9258a43e7f
Stefan Ferstl <st.ferstl@gmail.com>
2017-11-12 10:27:57 +0100

    #16 Remove elementFormDefault, attributeFormDefaul and xmlns

--- src/main/java/com/github/ferstl/maven/pomenforcers/model/package-info.java
+++ src/main/java/com/github/ferstl/maven/pomenforcers/model/package-info.java
@@ -14,17 +14,12 @@
  * limitations under the License.
  */
 @XmlSchema(
-    elementFormDefault=XmlNsForm.UNQUALIFIED,
-    attributeFormDefault=XmlNsForm.UNQUALIFIED,
     namespace = "http://maven.apache.org/POM/4.0.0",
-    location = "http://maven.apache.org/xsd/maven-4.0.0.xsd",
-    xmlns = @XmlNs(namespaceURI = "http://maven.apache.org/POM/4.0.0", prefix = ""))
+    location = "http://maven.apache.org/xsd/maven-4.0.0.xsd")
 @XmlAccessorType(FIELD)
 package com.github.ferstl.maven.pomenforcers.model;
 
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlNs;
-import javax.xml.bind.annotation.XmlNsForm;
 import javax.xml.bind.annotation.XmlSchema;
 
 import static javax.xml.bind.annotation.XmlAccessType.FIELD;
