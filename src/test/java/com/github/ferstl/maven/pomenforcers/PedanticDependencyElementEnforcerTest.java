c8b1e4631444cb8f75a95f6dbd16c1c9c4ee680b
Stefan Ferstl <st.ferstl@gmail.com>
2018-04-08 22:40:11 +0200

    #19 Improve test
    - add assertion
    - add test for custom ordering

--- src/test/java/com/github/ferstl/maven/pomenforcers/PedanticDependencyElementEnforcerTest.java
+++ src/test/java/com/github/ferstl/maven/pomenforcers/PedanticDependencyElementEnforcerTest.java
@@ -1,25 +1,63 @@
 package com.github.ferstl.maven.pomenforcers;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.apache.maven.enforcer.rule.api.EnforcerRuleHelper;
+import org.junit.Before;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import com.github.ferstl.maven.pomenforcers.model.ProjectModel;
 import com.github.ferstl.maven.pomenforcers.util.XmlUtils;
 
+import static com.github.ferstl.maven.pomenforcers.ErrorReportMatcher.hasErrors;
+import static com.github.ferstl.maven.pomenforcers.ErrorReportMatcher.hasNoErrors;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 
 public class PedanticDependencyElementEnforcerTest {
 
+  private ErrorReport errorReport;
+
+  @Before
+  public void before() {
+    this.errorReport = new ErrorReport(PedanticEnforcerRule.DEPENDENCY_ELEMENT);
+  }
+
   @Test
-  public void test() {
+  public void defaultOrdering() {
+    // arrange
+    Path pomFile = Paths.get("src/test/projects/example-project/module1/pom.xml");
+    PedanticDependencyElementEnforcer enforcer = createEnforcer(pomFile);
+
+    // act
+    enforcer.doEnforce(this.errorReport);
+
+    // assert
+    assertThat(this.errorReport, hasNoErrors());
+  }
+
+  @Test
+  public void customOrdering() {
+    // arrange
+    Path pomFile = Paths.get("src/test/projects/example-project/module1/pom.xml");
+    PedanticDependencyElementEnforcer enforcer = createEnforcer(pomFile);
+
+    enforcer.setElementPriorities("artifactId,groupId");
+
+    // act
+    enforcer.doEnforce(this.errorReport);
+
+    // assert
+    assertThat(this.errorReport, hasErrors());
+  }
+
+  private PedanticDependencyElementEnforcer createEnforcer(Path pomFile) {
+    Document document = XmlUtils.parseXml(pomFile.toFile());
     PedanticDependencyElementEnforcer enforcer = new PedanticDependencyElementEnforcer();
-    Document document = XmlUtils.parseXml(Paths.get("src/test/projects/example-project/module1/pom.xml").toFile());
 
     enforcer.initialize(mock(EnforcerRuleHelper.class), document, new ProjectModel());
-    ErrorReport report = new ErrorReport(PedanticEnforcerRule.DEPENDENCY_ELEMENT);
-    enforcer.doEnforce(report);
-
-    System.out.println(report);
+    return enforcer;
   }
+
+
 }
