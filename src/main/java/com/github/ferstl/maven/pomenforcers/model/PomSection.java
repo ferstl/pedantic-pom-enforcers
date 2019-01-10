4ce87b6c8ab4df729e63f8cb3d5a759856d2ca63
Stefan Ferstl <st.ferstl@gmail.com>
2019-01-11 00:32:19 +0100

    License stuff

--- src/main/java/com/github/ferstl/maven/pomenforcers/model/PomSection.java
+++ src/main/java/com/github/ferstl/maven/pomenforcers/model/PomSection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 - 2015 by Stefan Ferstl <st.ferstl@gmail.com>
+ * Copyright (c) 2012 - 2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
